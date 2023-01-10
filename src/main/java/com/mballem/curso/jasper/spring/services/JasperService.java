e44a57c23979de21e66f7744a45d3ec7a76e8a7a
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-01-10 17:53:56 -0300

    Atualização para carregar arquivos de imagem e jasper

--- src/main/java/com/mballem/curso/jasper/spring/services/JasperService.java
+++ src/main/java/com/mballem/curso/jasper/spring/services/JasperService.java
@@ -1,14 +1,16 @@
 package com.mballem.curso.jasper.spring.services;
 
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -17,29 +19,34 @@ import net.sf.jasperreports.engine.JasperPrint;
 
 @Service
 public class JasperService {
-	
+
 	private static final String JASPER_DIRETORIO = "classpath:jasper/";
 	private static final String JASPER_PREFIXO = "Funcionarios-";
 	private static final String JASPER_SUFIXO = ".jasper";
-	
+
 	@Autowired
 	private Connection connection;
 	
+	@Autowired
+	private ResourceLoader resourceLoader;
+
 	private Map<String, Object> params = new HashMap<>();
 
 	public void addParams(String key, Object value) {
 		this.params.put(key, value);
 	}
-	
+
 	public byte[] exportarPDF(String code) {
 		byte[] bytes = null;
 		try {
-			File file = ResourceUtils.getFile(JASPER_DIRETORIO.concat(JASPER_PREFIXO).concat(code).concat(JASPER_SUFIXO));
-			JasperPrint print = JasperFillManager.fillReport(file.getAbsolutePath(), params, connection);
+			Resource resource = resourceLoader
+					.getResource(JASPER_DIRETORIO.concat(JASPER_PREFIXO).concat(code).concat(JASPER_SUFIXO));
+			InputStream stream = resource.getInputStream();
+			JasperPrint print = JasperFillManager.fillReport(stream, params, connection);
 			bytes = JasperExportManager.exportReportToPdf(print);
-		} catch (FileNotFoundException | JRException e) {
+		} catch (IOException | JRException e) {
 			e.printStackTrace();
-		} 
+		}
 		return bytes;
 	}
 }
